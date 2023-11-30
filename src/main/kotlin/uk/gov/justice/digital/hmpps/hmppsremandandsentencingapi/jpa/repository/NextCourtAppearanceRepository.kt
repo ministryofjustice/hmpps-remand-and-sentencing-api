df91adee3fa31add25f8546952bbc614429ea7f8
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-11-30 08:57:45 +0000

    remove unused import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/repository/NextCourtAppearanceRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/repository/NextCourtAppearanceRepository.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.repository
 
 import org.springframework.data.repository.CrudRepository
-import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.CourtAppearanceEntity
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.NextCourtAppearanceEntity
 
 interface NextCourtAppearanceRepository : CrudRepository<NextCourtAppearanceEntity, Int>
