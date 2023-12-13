5a5385618c4368e89147c542ae4be1d634e1468f
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-12-13 14:00:50 +0000

    compile fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/ServiceUserService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/ServiceUserService.kt
@@ -1,17 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.service
 
-import kotlinx.coroutines.reactor.awaitSingle
-import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.config.AuthAwareAuthenticationToken
 
 @Service
 class ServiceUserService {
 
-  suspend fun getCurrentAuthentication(): AuthAwareAuthenticationToken = ReactiveSecurityContextHolder.getContext().awaitSingle().authentication as AuthAwareAuthenticationToken?
+  fun getCurrentAuthentication(): AuthAwareAuthenticationToken = SecurityContextHolder.getContext().authentication as AuthAwareAuthenticationToken?
     ?: throw IllegalStateException("User is not authenticated")
 
-  suspend fun getUsername(): String {
+  fun getUsername(): String {
     return getCurrentAuthentication().principal
   }
 }
