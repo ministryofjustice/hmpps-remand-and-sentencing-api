2a7afad41438ee7109050e8c3858bb59856e2a7f
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-01-04 13:54:51 +0000

    RASS-96 wrote a delete appearance test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/courtcase/UpdateCourtCaseTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/courtcase/UpdateCourtCaseTests.kt
@@ -35,6 +35,56 @@ class UpdateCourtCaseTests : IntegrationTestBase() {
   }
 
   @Test
+  fun `delete an appearance if emitted from list of appearances`() {
+    val charge = CreateCharge(UUID.randomUUID(), "OFF123", LocalDate.now(), null, "OUT123")
+    val appearance = CreateCourtAppearance(null, UUID.randomUUID(), "OUT123", "COURT1", "COURTREF1", LocalDate.now(), null, null, listOf(charge))
+    val secondAppearance = CreateCourtAppearance(null, UUID.randomUUID(), "OUT123", "COURT1", "COURTREF1", LocalDate.now().minusDays(7L), null, null, listOf(charge))
+    val courtCase = CreateCourtCase("PRISONER1", listOf(appearance, secondAppearance))
+    val courtCaseUuid = UUID.randomUUID()
+    webTestClient
+      .put()
+      .uri("/court-case/$courtCaseUuid")
+      .bodyValue(courtCase)
+      .headers {
+        it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING"))
+        it.contentType = MediaType.APPLICATION_JSON
+      }
+      .exchange()
+      .expectStatus()
+      .isOk
+
+    val courtCaseWithoutSecondAppearance = courtCase.copy(appearances = listOf(appearance))
+    webTestClient
+      .put()
+      .uri("/court-case/$courtCaseUuid")
+      .bodyValue(courtCaseWithoutSecondAppearance)
+      .headers {
+        it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING"))
+        it.contentType = MediaType.APPLICATION_JSON
+      }
+      .exchange()
+      .expectStatus()
+      .isOk
+
+    webTestClient
+      .get()
+      .uri("/court-case/$courtCaseUuid")
+      .headers {
+        it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING"))
+      }
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("$.latestAppearance.appearanceUuid")
+      .isEqualTo(appearance.appearanceUuid.toString())
+      .jsonPath("$.appearances.[?(@.appearanceUuid == '${secondAppearance.appearanceUuid}')]")
+      .doesNotExist()
+      .jsonPath("$.appearances.[?(@.appearanceUuid == '${appearance.appearanceUuid}')]")
+      .exists()
+  }
+
+  @Test
   fun `cannot edit prisoner id of a court case`() {
     val courtCase = createCourtCase()
     val editedCourtCase = courtCase.second.copy(prisonerId = "ADIFFERENTPRISONER")
