c1942b4a21a81fd7ded2577271e7236857111e48
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2024-09-16 14:59:42 +0100

    RASS-338 get sentence type by uuid (#130)
    * RASS-338 get sentence type by uuid
    
    * RASS-338 update doc

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/SentenceTypeService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/SentenceTypeService.kt
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.SentenceType
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.repository.SentenceTypeRepository
 import java.time.LocalDate
+import java.util.UUID
 
 @Service
 class SentenceTypeService(private val sentenceTypeRepository: SentenceTypeRepository) {
   fun search(age: Int, convictionDate: LocalDate): List<SentenceType> {
     return sentenceTypeRepository.findByAgeInAndConvictionDateIn(age, convictionDate).map { SentenceType.from(it) }
   }
+
+  fun findByUuid(sentenceTypeUuid: UUID): SentenceType? = sentenceTypeRepository.findBySentenceTypeUuid(sentenceTypeUuid)?.let { SentenceType.from(it) }
 }
