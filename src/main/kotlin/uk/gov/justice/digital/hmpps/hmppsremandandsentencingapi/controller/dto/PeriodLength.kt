f2da3bc84773911418d7d236d3c45e7a6914aec8
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-09-18 12:14:15 +0100

    RASS-348 change to multiple period lengths for sentences

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/PeriodLength.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/PeriodLength.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto
 
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.PeriodLengthEntity
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.PeriodLengthType
 
 data class PeriodLength(
   val years: Int?,
@@ -8,6 +9,7 @@ data class PeriodLength(
   val weeks: Int?,
   val days: Int?,
   val periodOrder: String,
+  val periodLengthType: PeriodLengthType
 ) {
   companion object {
     fun from(periodLengthEntity: PeriodLengthEntity): PeriodLength {
@@ -17,6 +19,7 @@ data class PeriodLength(
         periodLengthEntity.weeks,
         periodLengthEntity.days,
         periodLengthEntity.periodOrder,
+        periodLengthEntity.periodLengthType
       )
     }
   }
