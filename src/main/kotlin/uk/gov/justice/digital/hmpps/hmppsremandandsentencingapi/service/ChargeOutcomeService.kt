fe301e3da970435be3467835df64f79ac4da0daf
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-10-01 15:11:15 +0100

    RASS-360 get charge outcome by uuid

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/ChargeOutcomeService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/ChargeOutcomeService.kt
@@ -3,9 +3,12 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.service
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.ChargeOutcome
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.repository.ChargeOutcomeRepository
+import java.util.UUID
 
 @Service
 class ChargeOutcomeService(private val chargeOutcomeRepository: ChargeOutcomeRepository) {
 
   fun getAll(): List<ChargeOutcome> = chargeOutcomeRepository.findAll().map { ChargeOutcome.from(it) }
+
+  fun findByUuid(outcomeUuid: UUID): ChargeOutcome? = chargeOutcomeRepository.findByOutcomeUuid(outcomeUuid)?.let { ChargeOutcome.from(it) }
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/chargeoutcome/GetChargeOutcomeByUuidTests.kt
@@ -0,0 +1,55 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.chargeoutcome
+
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.IntegrationTestBase
+import java.util.UUID
+
+class GetChargeOutcomeByUuidTests : IntegrationTestBase() {
+
+  @Test
+  fun `get charge outcome by uuid`() {
+    webTestClient
+      .get()
+      .uri("/charge-outcome/315280e5-d53e-43b3-8ba6-44da25676ce2")
+      .headers {
+        it.authToken()
+      }
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("$.outcomeUuid")
+      .isEqualTo("315280e5-d53e-43b3-8ba6-44da25676ce2")
+      .jsonPath("$.outcomeName")
+      .isEqualTo("Remand in custody")
+      .jsonPath("$.nomisCode")
+      .isEqualTo("4531")
+      .jsonPath("$.outcomeType")
+      .isEqualTo("REMAND")
+      .jsonPath("$.displayOrder")
+      .isEqualTo(20)
+  }
+
+  @Test
+  fun `no charge outcome exist for uuid results in not found`() {
+    webTestClient
+      .get()
+      .uri("/charge-outcome/${UUID.randomUUID()}")
+      .headers {
+        it.authToken()
+      }
+      .exchange()
+      .expectStatus()
+      .isNotFound
+  }
+
+  @Test
+  fun `no token results in unauthorized`() {
+    webTestClient
+      .get()
+      .uri("/charge-outcome/315280e5-d53e-43b3-8ba6-44da25676ce2")
+      .exchange()
+      .expectStatus()
+      .isUnauthorized
+  }
+}
