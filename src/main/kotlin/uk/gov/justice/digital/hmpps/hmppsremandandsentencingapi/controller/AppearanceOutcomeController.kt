78b26483d1d36ee9cfda183e0d9c3a7d5610bd2f
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2024-10-01 08:57:37 +0100

    RASS-359 endpoint to get appearance outcome by uuid (#144)
    * RASS-359 endpoint to get appearance outcome by uuid
    
    * RASS-359 endpoint to get appearance outcome by uuid
    
    * RASS-359 format

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/AppearanceOutcomeController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/AppearanceOutcomeController.kt
@@ -4,14 +4,17 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.persistence.EntityNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.CourtAppearanceOutcome
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.service.AppearanceOutcomeService
+import java.util.UUID
 
 @RestController
 @RequestMapping("/appearance-outcome", produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -33,5 +36,23 @@ class AppearanceOutcomeController(private val appearanceOutcomeService: Appearan
   @ResponseStatus(HttpStatus.OK)
   fun getAllAppearanceOutcomes(): List<CourtAppearanceOutcome> {
     return appearanceOutcomeService.getAll()
+  }
+
+  @GetMapping("/{outcomeUuid}")
+  @Operation(
+    summary = "Get appearance outcome by UUID",
+    description = "This endpoint will retrieve appearance outcome by UUID",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(responseCode = "200", description = "Returns appearance outcome"),
+      ApiResponse(responseCode = "401", description = "Unauthorised, requires a valid Oauth2 token"),
+      ApiResponse(responseCode = "403", description = "Forbidden, requires an appropriate role"),
+      ApiResponse(responseCode = "404", description = "Not found if no appearance outcome at uuid"),
+    ],
+  )
+  @ResponseStatus(HttpStatus.OK)
+  fun getAppearanceOutcomeByUuid(@PathVariable outcomeUuid: UUID): CourtAppearanceOutcome {
+    return appearanceOutcomeService.findByUuid(outcomeUuid) ?: throw EntityNotFoundException("No appearance outcome found at $outcomeUuid")
   }
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/appearanceoutcome/GetAppearanceOutcomeByUuidTests.kt
@@ -0,0 +1,55 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.appearanceoutcome
+
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.IntegrationTestBase
+import java.util.UUID
+
+class GetAppearanceOutcomeByUuidTests : IntegrationTestBase() {
+
+  @Test
+  fun `get appearance outcome by uuid`() {
+    webTestClient
+      .get()
+      .uri("/appearance-outcome/2f585681-7b1a-44fb-a0cb-f9a4b1d9cda8")
+      .headers {
+        it.authToken()
+      }
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("$.outcomeUuid")
+      .isEqualTo("2f585681-7b1a-44fb-a0cb-f9a4b1d9cda8")
+      .jsonPath("$.outcomeName")
+      .isEqualTo("Remand in custody")
+      .jsonPath("$.nomisCode")
+      .isEqualTo("4531")
+      .jsonPath("$.outcomeType")
+      .isEqualTo("REMAND")
+      .jsonPath("$.displayOrder")
+      .isEqualTo(20)
+  }
+
+  @Test
+  fun `no appearance outcome exist for uuid results in not found`() {
+    webTestClient
+      .get()
+      .uri("/appearance-outcome/${UUID.randomUUID()}")
+      .headers {
+        it.authToken()
+      }
+      .exchange()
+      .expectStatus()
+      .isNotFound
+  }
+
+  @Test
+  fun `no token results in unauthorized`() {
+    webTestClient
+      .get()
+      .uri("/appearance-outcome/2f585681-7b1a-44fb-a0cb-f9a4b1d9cda8")
+      .exchange()
+      .expectStatus()
+      .isUnauthorized
+  }
+}
