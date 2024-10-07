c85fc764eca9e345fad7dcdf9b47a62c9e6a257d
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-10-07 13:38:51 +0100

    RASS-368 delete nomis outcome code and description when valid uuid supplied

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/ChargeLegacyData.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/ChargeLegacyData.kt
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter
-import com.fasterxml.jackson.annotation.JsonAnySetter
-
 data class ChargeLegacyData(
-  val outcomeReason: String?,
-  @JsonAnySetter
-  @get:JsonAnyGetter
-  val otherFields: Map<String, Any> = hashMapOf(),
+  val offenderChargeId: String?,
+  val bookingId: String?,
+  val postedDate: String?,
+  val nomisOutcomeCode: String?,
+  val outcomeDescription: String?,
 )
