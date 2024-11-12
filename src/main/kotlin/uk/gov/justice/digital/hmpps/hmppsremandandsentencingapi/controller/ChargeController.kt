9649c72d30e7e4bb7d23588e907dbb527acbb9cb
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-11-12 11:14:12 +0000

    RASS-343 doc fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/ChargeController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/ChargeController.kt
@@ -52,7 +52,7 @@ class ChargeController(private val chargeService: ChargeService) {
   )
   @ApiResponses(
     value = [
-      ApiResponse(responseCode = "200", description = "Returns court case UUID"),
+      ApiResponse(responseCode = "201", description = "Returns court case UUID"),
       ApiResponse(responseCode = "401", description = "Unauthorised, requires a valid Oauth2 token"),
       ApiResponse(responseCode = "403", description = "Forbidden, requires an appropriate role"),
     ],
