b5418cd47e1f9d932da923fb6fbd29ca69b988b2
Dan Benton <daniel.benton@digital.justice.gov.uk>
2024-11-13 15:59:46 +0000

    RCLL-186 Update test data to HDC_STANDARD_RECALL

--- src/test/resources/test_data/insert-recalls.sql
+++ src/test/resources/test_data/insert-recalls.sql
@@ -1,3 +1,3 @@
 TRUNCATE TABLE recall;
-INSERT INTO recall (recall_unique_identifier, prisoner_id, recall_date, return_to_custody_date, recall_type, created_at, created_by_username) VALUES ('550e8400-e29b-41d4-a716-446655440000', 'A12345B', '2024-07-01', '2024-07-01', 'HDC_RECALL', '2024-07-01 10:00:00+00', 'admin_user');
-INSERT INTO recall (recall_unique_identifier, prisoner_id, recall_date, return_to_custody_date, recall_type, created_at, created_by_username) VALUES ('550e8400-e29b-41d4-a716-446655440001', 'A12345B', '2024-07-02', '2024-07-02', 'HDC_RECALL', '2024-07-02 10:00:00+00', 'admin_user');
+INSERT INTO recall (recall_unique_identifier, prisoner_id, recall_date, return_to_custody_date, recall_type, created_at, created_by_username) VALUES ('550e8400-e29b-41d4-a716-446655440000', 'A12345B', '2024-07-01', '2024-07-01', 'HDC_STANDARD_RECALL', '2024-07-01 10:00:00+00', 'admin_user');
+INSERT INTO recall (recall_unique_identifier, prisoner_id, recall_date, return_to_custody_date, recall_type, created_at, created_by_username) VALUES ('550e8400-e29b-41d4-a716-446655440001', 'A12345B', '2024-07-02', '2024-07-02', 'HDC_STANDARD_RECALL', '2024-07-02 10:00:00+00', 'admin_user');
