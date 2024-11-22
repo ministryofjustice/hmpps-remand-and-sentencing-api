ef93a9d2f9a9d3cc4d7c28ce0225c146747f48e8
Dan Benton <daniel.benton@digital.justice.gov.uk>
2024-11-22 15:22:26 +0000

    RASS-500 Surface court case status

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/CourtCase.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/CourtCase.kt
@@ -7,6 +7,7 @@ import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.EntityS
 data class CourtCase(
   val prisonerId: String,
   val courtCaseUuid: String,
+  val status: EntityStatus,
   val latestAppearance: CourtAppearance?,
   val appearances: List<CourtAppearance>,
   val legacyData: JsonNode?,
@@ -16,6 +17,7 @@ data class CourtCase(
       return CourtCase(
         courtCaseEntity.prisonerId,
         courtCaseEntity.caseUniqueIdentifier,
+        courtCaseEntity.statusId,
         courtCaseEntity.latestCourtAppearance?.let { CourtAppearance.from(it) },
         courtCaseEntity.appearances.filter { it.statusId == EntityStatus.ACTIVE }.map { CourtAppearance.from(it) },
         courtCaseEntity.legacyData,
