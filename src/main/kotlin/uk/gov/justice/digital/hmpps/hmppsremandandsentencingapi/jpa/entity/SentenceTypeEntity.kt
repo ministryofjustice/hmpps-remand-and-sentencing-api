c481d4680783fd31efe2df0a4b7a0b428097a8a0
Pavilion Sahota <pavilion.sahota@justice.gov.uk>
2024-11-28 14:17:25 +0000

    Fixed lint error

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/SentenceTypeEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/SentenceTypeEntity.kt
@@ -25,5 +25,5 @@ class SentenceTypeEntity(
   val maxDateExclusive: LocalDate?,
   @Enumerated(EnumType.STRING)
   val classification: SentenceTypeClassification,
-  val hint: String
+  val hint: String,
 )
