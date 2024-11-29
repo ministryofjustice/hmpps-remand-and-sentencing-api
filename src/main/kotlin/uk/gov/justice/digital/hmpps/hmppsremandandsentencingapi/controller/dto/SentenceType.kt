5545ee863b8b63b404be32d7f29f2e7d65e7a690
Pavilion Sahota <159157649+pavilionsahota@users.noreply.github.com>
2024-11-29 10:12:07 +0000

    RASS-534 Added new migration to update sentence type table (#237)
    * Added new migration to update sentence type table
    
    * Fixed unit tests
    
    * Fixed lint error
    
    * Fixed SQL migration and entity
    
    * Updated entity property name
    
    * Fixed tests
    
    * Fixed tests
    
    * Fixed test
    
    * Added hintText property to Dto
    
    * Made new field nullable
    
    * Fixed typo
    
    ---------
    
    Co-authored-by: Pavilion Sahota <pavilion.sahota@justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/SentenceType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/SentenceType.kt
@@ -8,10 +8,11 @@ data class SentenceType(
   val sentenceTypeUuid: UUID,
   val description: String,
   val classification: SentenceTypeClassification,
+  val hintText: String?,
 ) {
   companion object {
     fun from(sentenceTypeEntity: SentenceTypeEntity): SentenceType {
-      return SentenceType(sentenceTypeEntity.sentenceTypeUuid, sentenceTypeEntity.description, sentenceTypeEntity.classification)
+      return SentenceType(sentenceTypeEntity.sentenceTypeUuid, sentenceTypeEntity.description, sentenceTypeEntity.classification, sentenceTypeEntity.hintText)
     }
   }
 }
