b617fe90ee1952d234129d0fb7f4aaea56690324
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2024-12-10 17:01:54 +0000

    RASS-585 get all appearance type by uuid endpoint (#254)
    * RASS-411 create appearance type table
    
    * RASS-582 get all appearance types endpoint
    
    * RASS-585 get all appearance type by uuid endpoint
    
    * RASS-585 get all appearance type by uuid endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/AppearanceTypeController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/AppearanceTypeController.kt
@@ -4,12 +4,15 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.persistence.EntityNotFoundException
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.AppearanceType
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.service.AppearanceTypeService
+import java.util.UUID
 
 @RestController
 @RequestMapping("/appearance-type", produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -27,7 +30,19 @@ class AppearanceTypeController(private val appearanceTypeService: AppearanceType
       ApiResponse(responseCode = "401", description = "Unauthorised, requires a valid Oauth2 token"),
     ],
   )
-  fun getAllAppearanceTypes(): List<AppearanceType> {
-    return appearanceTypeService.getAll()
-  }
+  fun getAllAppearanceTypes(): List<AppearanceType> = appearanceTypeService.getAll()
+
+  @GetMapping("/{appearanceTypeUuid}")
+  @Operation(
+    summary = "Get appearance type by uuid",
+    description = "This endpoint will get appearance type by uuid",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(responseCode = "200", description = "Returns appearance type"),
+      ApiResponse(responseCode = "404", description = "Appearance type not found"),
+      ApiResponse(responseCode = "401", description = "Unauthorised, requires a valid Oauth2 token"),
+    ],
+  )
+  fun getAppearanceTypeById(@PathVariable appearanceTypeUuid: UUID): AppearanceType = appearanceTypeService.findByUuid(appearanceTypeUuid) ?: throw EntityNotFoundException("No appearance type found at $appearanceTypeUuid")
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/AppearanceTypeService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/AppearanceTypeService.kt
@@ -3,9 +3,12 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.service
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.AppearanceType
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.repository.AppearanceTypeRepository
+import java.util.UUID
 
 @Service
 class AppearanceTypeService(private val appearanceTypeRepository: AppearanceTypeRepository) {
 
   fun getAll(): List<AppearanceType> = appearanceTypeRepository.findAll().map { AppearanceType.from(it) }
+
+  fun findByUuid(appearanceTypeUuid: UUID): AppearanceType? = appearanceTypeRepository.findByAppearanceTypeUuid(appearanceTypeUuid)?.let { AppearanceType.from(it) }
 }
