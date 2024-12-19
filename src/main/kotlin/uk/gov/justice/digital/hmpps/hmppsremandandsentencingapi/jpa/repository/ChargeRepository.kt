b83dc8cea9b5c2f4fc400f66033c3c8bd4397de8
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-12-19 09:36:46 +0000

    RASS-633 update charge in all appearances change

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/repository/ChargeRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/repository/ChargeRepository.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.repository
 
 import org.springframework.data.repository.CrudRepository
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.ChargeEntity
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.EntityStatus
 import java.util.UUID
 
 interface ChargeRepository : CrudRepository<ChargeEntity, Int> {
@@ -10,4 +11,6 @@ interface ChargeRepository : CrudRepository<ChargeEntity, Int> {
   fun findFirstByLifetimeChargeUuidOrderByCreatedAtDesc(lifetimeUUID: UUID): ChargeEntity?
 
   fun findFirstByCourtAppearancesLifetimeUuidAndLifetimeChargeUuidOrderByCreatedAtDesc(courtAppearanceLifetimeUuid: UUID, lifetimeUUID: UUID): ChargeEntity?
+
+  fun findByLifetimeChargeUuidAndStatusId(lifetimeUUID: UUID, status: EntityStatus): List<ChargeEntity>
 }
