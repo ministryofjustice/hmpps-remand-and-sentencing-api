a413794b4daddece03e219998a56f097ee1668a4
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2025-01-03 08:48:30 +0000

    RASS-636 add appearance time to legacy data (#271)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/legacy/controller/dto/CourtAppearanceLegacyData.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/legacy/controller/dto/CourtAppearanceLegacyData.kt
@@ -1,6 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.legacy.controller.dto
 
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 data class CourtAppearanceLegacyData(
   val eventId: String?,
@@ -9,4 +12,31 @@ data class CourtAppearanceLegacyData(
   val nomisOutcomeCode: String?,
   val outcomeDescription: String?,
   val nextEventDateTime: LocalDateTime?,
-)
+  val appearanceTime: LocalTime?,
+) {
+
+  fun copyFrom(appearanceTime: LocalTime?): CourtAppearanceLegacyData {
+    return CourtAppearanceLegacyData(
+      eventId,
+      caseId,
+      LocalDate.now().format(DateTimeFormatter.ISO_DATE),
+      nomisOutcomeCode,
+      outcomeDescription,
+      nextEventDateTime,
+      appearanceTime,
+    )
+  }
+  companion object {
+    fun from(appearanceTime: LocalTime): CourtAppearanceLegacyData {
+      return CourtAppearanceLegacyData(
+        null,
+        null,
+        LocalDate.now().format(DateTimeFormatter.ISO_DATE),
+        null,
+        null,
+        null,
+        appearanceTime,
+      )
+    }
+  }
+}
