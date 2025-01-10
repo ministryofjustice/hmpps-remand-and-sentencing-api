fe1bb4828b45d10cd84b2c687658eaefbdc16142
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2025-01-10 15:45:54 +0000

    RASS-668 add legacy data column to period length (#286)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/legacy/controller/dto/PeriodLengthLegacyData.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.legacy.controller.dto
+
+data class PeriodLengthLegacyData(
+  val lifeSentence: Boolean?,
+  val sentenceTermCode: String?,
+)
