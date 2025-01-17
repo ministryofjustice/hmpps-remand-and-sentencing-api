6d5937eb6d0af75b04bc872d3c6da100a472e459
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2025-01-17 12:53:44 +0000

    multiple consecutive sentences to the same sentence

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/courtcase/CreateCourtCaseTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/courtcase/CreateCourtCaseTests.kt
@@ -4,6 +4,7 @@ import org.hamcrest.text.MatchesPattern
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.PeriodLengthType
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.util.DpsDataCreator
 
 class CreateCourtCaseTests : IntegrationTestBase() {
@@ -30,6 +31,80 @@ class CreateCourtCaseTests : IntegrationTestBase() {
       .jsonPath("$.charges[0].chargeUuid")
       .isEqualTo(createCourtCase.appearances.first().charges.first().chargeUuid.toString())
     expectInsertedMessages(createCourtCase.prisonerId)
+  }
+
+  @Test
+  fun `create multiple consecutive to sentences for one forthwith sentence`() {
+    val forthwithSentence = DpsDataCreator.dpsCreateSentence(
+      chargeNumber = "1",
+      sentenceServeType = "FORTHWITH",
+      periodLengths = listOf(
+        DpsDataCreator.dpsCreatePeriodLength(years = 4, type = PeriodLengthType.SENTENCE_LENGTH),
+      ),
+    )
+    val firstConcurrentGroupOne = DpsDataCreator.dpsCreateSentence(
+      chargeNumber = "2",
+      sentenceServeType = "CONCURRENT",
+      periodLengths = listOf(
+        DpsDataCreator.dpsCreatePeriodLength(months = 27, periodOrder = "months", type = PeriodLengthType.SENTENCE_LENGTH),
+      ),
+    )
+    val secondConcurrentGroupOne = DpsDataCreator.dpsCreateSentence(
+      chargeNumber = "3",
+      sentenceServeType = "CONCURRENT",
+      periodLengths = listOf(
+        DpsDataCreator.dpsCreatePeriodLength(months = 27, periodOrder = "months", type = PeriodLengthType.SENTENCE_LENGTH),
+      ),
+    )
+
+    val firstConcurrentGroupTwo = DpsDataCreator.dpsCreateSentence(
+      chargeNumber = "4",
+      sentenceServeType = "CONSECUTIVE",
+      consecutiveToChargeNumber = "1",
+      periodLengths = listOf(
+        DpsDataCreator.dpsCreatePeriodLength(months = 27, periodOrder = "months", type = PeriodLengthType.SENTENCE_LENGTH),
+      ),
+    )
+
+    val secondConcurrentGroupTwo = DpsDataCreator.dpsCreateSentence(
+      chargeNumber = "5",
+      sentenceServeType = "CONSECUTIVE",
+      consecutiveToChargeNumber = "1",
+      periodLengths = listOf(
+        DpsDataCreator.dpsCreatePeriodLength(months = 27, periodOrder = "months", type = PeriodLengthType.SENTENCE_LENGTH),
+      ),
+    )
+
+    val thirdConcurrentGroupTwo = DpsDataCreator.dpsCreateSentence(
+      chargeNumber = "6",
+      sentenceServeType = "CONSECUTIVE",
+      consecutiveToChargeNumber = "1",
+      periodLengths = listOf(
+        DpsDataCreator.dpsCreatePeriodLength(months = 27, periodOrder = "months", type = PeriodLengthType.SENTENCE_LENGTH),
+      ),
+    )
+
+    val firstCharge = DpsDataCreator.dpsCreateCharge(sentence = forthwithSentence)
+    val secondCharge = DpsDataCreator.dpsCreateCharge(sentence = firstConcurrentGroupOne)
+    val thirdCharge = DpsDataCreator.dpsCreateCharge(sentence = secondConcurrentGroupOne)
+    val fourthCharge = DpsDataCreator.dpsCreateCharge(sentence = firstConcurrentGroupTwo)
+    val fifthCharge = DpsDataCreator.dpsCreateCharge(sentence = secondConcurrentGroupTwo)
+    val sixthCharge = DpsDataCreator.dpsCreateCharge(sentence = thirdConcurrentGroupTwo)
+
+    val appearance = DpsDataCreator.dpsCreateCourtAppearance(warrantType = "SENTENCING", charges = listOf(firstCharge, secondCharge, thirdCharge, fourthCharge, fifthCharge, sixthCharge))
+    val courtCase = DpsDataCreator.dpsCreateCourtCase(appearances = listOf(appearance))
+
+    webTestClient
+      .post()
+      .uri("/court-case")
+      .bodyValue(courtCase)
+      .headers {
+        it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING"))
+        it.contentType = MediaType.APPLICATION_JSON
+      }
+      .exchange()
+      .expectStatus()
+      .isCreated
   }
 
   @Test
