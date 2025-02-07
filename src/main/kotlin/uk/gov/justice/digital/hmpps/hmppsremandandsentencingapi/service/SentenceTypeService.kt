19cad87858e82e2d9aa3abecdc82a2934e17eaaa
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2025-02-07 10:41:03 +0000

    RASS-730 add recall sentence type bucket (#321)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/SentenceTypeService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/SentenceTypeService.kt
@@ -2,13 +2,19 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.service
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.SentenceType
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.SentenceTypeClassification
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.repository.SentenceTypeRepository
 import java.time.LocalDate
 import java.util.UUID
 
 @Service
 class SentenceTypeService(private val sentenceTypeRepository: SentenceTypeRepository) {
-  fun search(age: Int, convictionDate: LocalDate): List<SentenceType> = sentenceTypeRepository.findByAgeInAndConvictionDateIn(age, convictionDate).map { SentenceType.from(it) }
+
+  fun search(age: Int, convictionDate: LocalDate): List<SentenceType> = sentenceTypeRepository.findByAgeInAndConvictionDateInAndClassificationNot(
+    age,
+    convictionDate,
+    SentenceTypeClassification.LEGACY_RECALL,
+  ).map { SentenceType.from(it) }
 
   fun findByUuid(sentenceTypeUuid: UUID): SentenceType? = sentenceTypeRepository.findBySentenceTypeUuid(sentenceTypeUuid)?.let { SentenceType.from(it) }
 
