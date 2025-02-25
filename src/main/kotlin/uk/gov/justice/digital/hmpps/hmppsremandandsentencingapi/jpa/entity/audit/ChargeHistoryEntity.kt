04f931e773c052c8e33ce03fff559f7b27bb5b19
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2025-02-25 16:59:29 +0000

    RASS-745 add charge history table (#348)
    * RASS-745 add charge history table
    
    * RASS-745 add charge history table
    
    * RASS-745 add charge history table

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/audit/ChargeHistoryEntity.kt
@@ -0,0 +1,48 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.audit
+
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.ChargeEntity
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.EntityStatus
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.legacy.controller.dto.ChargeLegacyData
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import java.util.UUID
+
+@Entity
+@Table(name = "charge_history")
+class ChargeHistoryEntity(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Int = 0,
+  var chargeUuid: UUID,
+  val offenceCode: String,
+  val offenceStartDate: LocalDate?,
+  val offenceEndDate: LocalDate?,
+  @Enumerated(EnumType.ORDINAL)
+  var statusId: EntityStatus,
+  val chargeOutcomeId: Int?,
+  var supersedingChargeId: Int,
+  val terrorRelated: Boolean?,
+  val createdAt: ZonedDateTime = ZonedDateTime.now(),
+  val createdBy: String,
+  val createdPrison: String?,
+  val updatedAt: ZonedDateTime?,
+  val updatedBy: String?,
+  val updatedPrison: String?,
+  @JdbcTypeCode(SqlTypes.JSON)
+  var legacyData: ChargeLegacyData? = null,
+  val mergedFromCaseId: Int?,
+  @OneToOne
+  @JoinColumn(name = "original_charge_id")
+  val originalCharge: ChargeEntity,
+)
