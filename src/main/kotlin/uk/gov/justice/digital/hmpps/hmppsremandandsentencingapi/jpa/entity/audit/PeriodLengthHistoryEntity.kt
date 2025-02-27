ed3b1a3d0edbb71c2c8411ae5f9c1f114d89abcd
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2025-02-27 13:40:43 +0000

    RASS-780 add period length history table (#352)
    * RASS-780 add period length history table
    
    * RASS-780 add period length history table

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/audit/PeriodLengthHistoryEntity.kt
@@ -0,0 +1,50 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.audit
+
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.PeriodLengthEntity
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.EntityStatus
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.PeriodLengthType
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.legacy.controller.dto.PeriodLengthLegacyData
+import java.time.ZonedDateTime
+import java.util.UUID
+
+@Entity
+@Table(name = "period_length_history")
+class PeriodLengthHistoryEntity(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Int = 0,
+  val periodLengthUuid: UUID,
+  val years: Int?,
+  val months: Int?,
+  val weeks: Int?,
+  val days: Int?,
+  val periodOrder: String,
+  @Enumerated(EnumType.STRING)
+  val periodLengthType: PeriodLengthType,
+  val sentenceId: Int?,
+  val appearanceId: Int?,
+  @JdbcTypeCode(SqlTypes.JSON)
+  val legacyData: PeriodLengthLegacyData?,
+  @Enumerated(EnumType.ORDINAL)
+  val statusId: EntityStatus,
+  val createdAt: ZonedDateTime,
+  val createdBy: String,
+  val createdPrison: String?,
+  val updatedAt: ZonedDateTime?,
+  val updatedBy: String?,
+  val updatedPrison: String?,
+  @OneToOne
+  @JoinColumn(name = "original_period_length_id")
+  val originalPeriodLength: PeriodLengthEntity,
+)
