6e705b9ec3a2b662cdd7656b401e9210bb2d3cad
Md Hasan Rahman <hassan.rahman@justice.gov.uk>
2025-03-10 13:46:41 +0000

    updating comments

--- src/main/resources/migration/common/V64__update_charge_outcome_table.sql
+++ src/main/resources/migration/common/V64__update_charge_outcome_table.sql
@@ -1,8 +1,8 @@
--- AC1: Drop the is_sub_list column
+-- Drop the is_sub_list column
 ALTER TABLE charge_outcome
 DROP COLUMN is_sub_list;
 
--- AC2: Update outcome_type to NON_CUSTODIAL for specific charge outcomes based on outcome_name
+-- Update outcome_type to NON_CUSTODIAL for specific charge outcomes based on outcome_name
 UPDATE charge_outcome
 SET outcome_type = 'NON_CUSTODIAL'
 WHERE outcome_name IN (
