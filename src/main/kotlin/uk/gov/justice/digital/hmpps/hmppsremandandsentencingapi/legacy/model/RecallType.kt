b709fb21ba063287295602d37a7ae9460f2f9f07
Joel Stobart (MOJ) <87521440+joelstobart-moj@users.noreply.github.com>
2025-04-01 15:29:45 +0100

    Rass 913 shape of recalls (#384)
    * * Small changes to use a parameter for the calls
    * Change tests for parameters
    * Introduce coverage for sorted values
    
    * * Return the active and expiry in the summary
    * Update some data elements
    * Test sorting
    
    * * Small fix to the shape of the recalls
    
    ---------
    
    Co-authored-by: Joel Stobart <>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/legacy/model/RecallType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/legacy/model/RecallType.kt
@@ -3,14 +3,22 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.legacy.model
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@Schema(
+  description = "Recall type including name, length in days (if fixed term), and a fixed-term indicator",
+  example = """{"name": "FIXED_TERM_RECALL_14", "lengthInDays": 14, "isFixedTermRecall": true}""",
+)
 enum class RecallType(
+  @get:Schema(description = "Recall length in days", example = "14")
   val lengthInDays: Int? = null,
+
+  @get:Schema(description = "Whether this recall is a fixed term", example = "true")
   val isFixedTermRecall: Boolean = false,
 ) {
-  STANDARD_RECALL(),
-  STANDARD_RECALL_255(),
+  STANDARD_RECALL,
+  STANDARD_RECALL_255,
   FIXED_TERM_RECALL_14(14, true),
   FIXED_TERM_RECALL_28(28, true),
   NONE,
@@ -20,7 +28,8 @@ enum class RecallType(
     @JsonCreator
     @JvmStatic
     fun fromJson(@JsonProperty("name") name: String?): RecallType = name?.let { entries.firstOrNull { it.name == name } } ?: NONE
-    fun from(classification: String): RecallType = RecallType.entries.firstOrNull { it.name == classification } ?: NONE
+
+    fun from(classification: String): RecallType = entries.firstOrNull { it.name == classification } ?: NONE
   }
 
   @JsonProperty("name")
