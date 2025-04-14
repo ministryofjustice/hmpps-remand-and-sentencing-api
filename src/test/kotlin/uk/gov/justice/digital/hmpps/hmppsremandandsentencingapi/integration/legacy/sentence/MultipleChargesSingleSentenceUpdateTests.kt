cc19e6cd6c66102d99f4a448c9da8885888424d1
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2025-04-14 13:20:34 +0100

    RASS-953 update all sentence records when an update comes through

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/legacy/sentence/MultipleChargesSingleSentenceUpdateTests.kt
@@ -0,0 +1,84 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.legacy.sentence
+
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.CreateCourtCase
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.legacy.util.DataCreator
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.legacy.controller.dto.LegacyCreateSentence
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.legacy.controller.dto.LegacySentenceCreatedResponse
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.util.DpsDataCreator
+
+class MultipleChargesSingleSentenceUpdateTests : IntegrationTestBase() {
+
+  @Test
+  fun `update sentence with multiple charges`() {
+    val sentenceWithMultipleCharges = createSentenceWithMultipleCharges()
+    val updatedLegacyData = sentenceWithMultipleCharges.legacySentence.legacyData.copy(sentenceCalcType = "ADIMP", sentenceCategory = "2020") // DPS SDS sentence type
+    val updatedSentence = sentenceWithMultipleCharges.legacySentence.copy(legacyData = updatedLegacyData, fine = null)
+    webTestClient
+      .put()
+      .uri("/legacy/sentence/${sentenceWithMultipleCharges.legacySentenceResponse.lifetimeUuid}")
+      .bodyValue(updatedSentence)
+      .headers {
+        it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING_SENTENCE_RW"))
+        it.contentType = MediaType.APPLICATION_JSON
+      }
+      .exchange()
+      .expectStatus()
+      .isNoContent
+
+    val courtCaseUuid = sentenceWithMultipleCharges.courtCaseUuid
+    val (firstCharge, secondCharge) = sentenceWithMultipleCharges.courtCase.appearances.first().charges
+
+    webTestClient
+      .get()
+      .uri("/court-case/$courtCaseUuid")
+      .headers {
+        it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING"))
+      }
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("$.appearances[*].charges[?(@.chargeUuid == '${firstCharge.chargeUuid}')].sentence.sentenceType.sentenceTypeUuid")
+      .isEqualTo("02fe3513-40a6-47e9-a72d-9dafdd936a0e")
+      .jsonPath("$.appearances[*].charges[?(@.chargeUuid == '${secondCharge.chargeUuid}')].sentence.sentenceType.sentenceTypeUuid")
+      .isEqualTo("02fe3513-40a6-47e9-a72d-9dafdd936a0e")
+  }
+
+  fun createSentenceWithMultipleCharges(): TestData {
+    val firstCharge = DpsDataCreator.dpsCreateCharge(sentence = null)
+    val secondCharge = DpsDataCreator.dpsCreateCharge(sentence = null)
+    val thirdCharge = DpsDataCreator.dpsCreateCharge(sentence = null)
+    val appearance = DpsDataCreator.dpsCreateCourtAppearance(charges = listOf(firstCharge, secondCharge, thirdCharge))
+    val courtCase = DpsDataCreator.dpsCreateCourtCase(appearances = listOf(appearance))
+    val (courtCaseUuid) = createCourtCase(courtCase)
+    val legacySentence = DataCreator.legacyCreateSentence(chargeUuids = listOf(firstCharge.chargeUuid, secondCharge.chargeUuid))
+    val response = webTestClient
+      .post()
+      .uri("/legacy/sentence")
+      .bodyValue(legacySentence)
+      .headers {
+        it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING_SENTENCE_RW"))
+        it.contentType = MediaType.APPLICATION_JSON
+      }
+      .exchange()
+      .expectStatus()
+      .isCreated.returnResult(LegacySentenceCreatedResponse::class.java)
+      .responseBody.blockFirst()!!
+    return TestData(
+      courtCase,
+      courtCaseUuid,
+      legacySentence,
+      response,
+    )
+  }
+}
+
+data class TestData(
+  val courtCase: CreateCourtCase,
+  val courtCaseUuid: String,
+  val legacySentence: LegacyCreateSentence,
+  val legacySentenceResponse: LegacySentenceCreatedResponse,
+)
