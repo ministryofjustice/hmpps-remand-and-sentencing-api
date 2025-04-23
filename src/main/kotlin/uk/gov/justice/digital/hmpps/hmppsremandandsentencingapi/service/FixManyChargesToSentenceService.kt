f7cfcd3c7401d167cff49b0472fe9adc17bde9dd
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2025-04-23 14:40:52 +0100

    RASS-922 split out fixSentences

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/FixManyChargesToSentenceService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/FixManyChargesToSentenceService.kt
@@ -6,6 +6,7 @@ import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.domain.EventType
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.domain.RecordEventMetadata
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.domain.util.EventMetadataCreator
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.CourtCaseEntity
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.PeriodLengthEntity
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.SentenceEntity
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.audit.PeriodLengthHistoryEntity
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.audit.SentenceHistoryEntity
@@ -38,47 +39,40 @@ class FixManyChargesToSentenceService(private val sentenceHistoryRepository: Sen
       .groupByTo(mutableMapOf()) { it.record.sentenceUuid }
     val eventsToEmit = mutableSetOf<EventMetadata>()
     toFixSentences.forEach { (_, sentenceRecords) ->
-      val (firstRecord, eventMetadata) = sentenceRecords.removeFirst()
-      firstRecord.statusId = if (firstRecord.legacyData?.active == false) EntityStatus.INACTIVE else EntityStatus.ACTIVE
-      sentenceHistoryRepository.save(SentenceHistoryEntity.from(firstRecord))
-      firstRecord.periodLengths.filter { it.statusId == EntityStatus.MANY_CHARGES_DATA_FIX }
-        .forEach { firstRecordPeriodLength ->
-          firstRecordPeriodLength.statusId = EntityStatus.ACTIVE
-          periodLengthHistoryRepository.save(PeriodLengthHistoryEntity.from(firstRecordPeriodLength))
-        }
+      val firstSentenceRecordEventMetadata = sentenceRecords.removeFirst()
+      val firstSentenceEventToEmit = fixSentence(firstSentenceRecordEventMetadata, EventType.SENTENCE_UPDATED, firstSentenceRecordEventMetadata.record.sentenceUuid)
+      eventsToEmit.add(firstSentenceEventToEmit)
+      fixPeriodLengths(firstSentenceRecordEventMetadata.record.periodLengths)
 
-      eventsToEmit.add(
-        EventMetadataCreator.sentenceEventMetadata(
-          eventMetadata.prisonerId,
-          eventMetadata.courtCaseId!!,
-          eventMetadata.chargeId!!,
-          firstRecord.sentenceUuid.toString(),
-          eventMetadata.courtAppearanceId!!,
-          EventType.SENTENCE_UPDATED,
-        ),
-      )
-
-      sentenceRecords.forEach { (sentenceRecord, eventMetadata) ->
-        sentenceRecord.sentenceUuid = UUID.randomUUID()
-        sentenceRecord.statusId = if (sentenceRecord.legacyData?.active == false) EntityStatus.INACTIVE else EntityStatus.ACTIVE
-        sentenceHistoryRepository.save(SentenceHistoryEntity.from(sentenceRecord))
-        eventsToEmit.add(
-          EventMetadataCreator.sentenceEventMetadata(
-            eventMetadata.prisonerId,
-            eventMetadata.courtCaseId!!,
-            eventMetadata.chargeId!!,
-            sentenceRecord.sentenceUuid.toString(),
-            eventMetadata.courtAppearanceId!!,
-            EventType.SENTENCE_INSERTED,
-          ),
-        )
-        sentenceRecord.periodLengths.forEach { periodLengthRecord ->
-          periodLengthRecord.periodLengthUuid = UUID.randomUUID()
-          periodLengthRecord.statusId = EntityStatus.ACTIVE
-          periodLengthHistoryRepository.save(PeriodLengthHistoryEntity.from(periodLengthRecord))
-        }
+      sentenceRecords.forEach { sentenceRecordEventMetadata ->
+        val sentenceEventToEmit = fixSentence(sentenceRecordEventMetadata, EventType.SENTENCE_INSERTED, UUID.randomUUID())
+        eventsToEmit.add(sentenceEventToEmit)
+        fixPeriodLengths(sentenceRecordEventMetadata.record.periodLengths)
       }
     }
     return eventsToEmit
+  }
+
+  private fun fixSentence(sentenceRecordEventMetadata: RecordEventMetadata<SentenceEntity>, sentenceEventType: EventType, sentenceUuid: UUID): EventMetadata {
+    val (sentenceRecord, eventMetadata) = sentenceRecordEventMetadata
+    sentenceRecord.statusId = if (sentenceRecord.legacyData?.active == false) EntityStatus.INACTIVE else EntityStatus.ACTIVE
+    sentenceRecord.sentenceUuid = sentenceUuid
+    sentenceHistoryRepository.save(SentenceHistoryEntity.from(sentenceRecord))
+    return EventMetadataCreator.sentenceEventMetadata(
+      eventMetadata.prisonerId,
+      eventMetadata.courtCaseId!!,
+      eventMetadata.chargeId!!,
+      sentenceRecord.sentenceUuid.toString(),
+      eventMetadata.courtAppearanceId!!,
+      sentenceEventType,
+    )
+  }
+
+  private fun fixPeriodLengths(periodLengths: List<PeriodLengthEntity>) {
+    periodLengths.filter { it.statusId == EntityStatus.MANY_CHARGES_DATA_FIX }
+      .forEach { firstRecordPeriodLength ->
+        firstRecordPeriodLength.statusId = EntityStatus.ACTIVE
+        periodLengthHistoryRepository.save(PeriodLengthHistoryEntity.from(firstRecordPeriodLength))
+      }
   }
 }
