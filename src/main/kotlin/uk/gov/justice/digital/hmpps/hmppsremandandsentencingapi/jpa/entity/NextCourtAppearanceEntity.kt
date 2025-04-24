4982a03a1027214bb549fb2b9c2f5e126de80af3
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2025-04-24 17:04:56 +0100

    RASS-964 return less data, introduce indexes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/NextCourtAppearanceEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/NextCourtAppearanceEntity.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -30,7 +31,7 @@ class NextCourtAppearanceEntity(
   @OneToOne
   @JoinColumn(name = "appearance_type_id")
   val appearanceType: AppearanceTypeEntity,
-  @OneToOne
+  @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "future_skeleton_appearance_id")
   val futureSkeletonAppearance: CourtAppearanceEntity,
 ) {
