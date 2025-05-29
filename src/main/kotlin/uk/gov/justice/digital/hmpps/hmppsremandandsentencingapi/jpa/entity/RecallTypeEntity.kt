dea3af58184a883a367a39461d8ef8dca5cf8ce5
ldlharper <ldlharper@gmail.com>
2025-05-29 16:01:46 +0100

    RCLL-376 Recall Type + DPS sentence type mapping to nomis recall sentence types. (#482)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/RecallTypeEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/RecallTypeEntity.kt
@@ -20,38 +20,68 @@ class RecallTypeEntity(
   val code: RecallType,
   val description: String,
 ) {
-  // TODO RCLL-376 confirm all DPS to NOMIS recall types.
+
   fun toLegacySentenceType(sentenceType: SentenceTypeEntity): Pair<String, String> = (
     when (this.code) {
       RecallType.LR -> findLicenseRecallLegacySentenceType(sentenceType)
       RecallType.FTR_14 -> "14FTR_ORA"
-      RecallType.FTR_28 -> "FTR"
+      RecallType.FTR_28 -> if (sentenceType.nomisSentenceCalcType.contains("ORA")) "FTR_ORA" else "FTR"
+      // TODO RCLL-399 Check LR_HDC types.
       RecallType.LR_HDC -> "LR"
       RecallType.FTR_HDC_14 -> "14FTRHDC_ORA"
       RecallType.FTR_HDC_28 -> "FTR_HDC"
-      RecallType.CUR_HDC -> "CUR"
-      RecallType.IN_HDC -> "HDR"
+      RecallType.CUR_HDC -> if (sentenceType.nomisSentenceCalcType.contains("ORA")) "CUR_ORA" else "CUR"
+      RecallType.IN_HDC -> if (sentenceType.nomisSentenceCalcType.contains("ORA")) "HDR_ORA" else "HDR"
     }
     ) to sentenceType.nomisCjaCode
 
   private fun findLicenseRecallLegacySentenceType(sentenceType: SentenceTypeEntity): String = when (sentenceType.classification) {
-    SentenceTypeClassification.STANDARD -> "LR"
-    SentenceTypeClassification.EXTENDED -> {
-      if (sentenceType.nomisSentenceCalcType == "LASPO_AR") {
-        "LR_LASPO_AR"
-      } else if (sentenceType.nomisSentenceCalcType == "LASPO_DR") {
-        "LR_LASPO_DR"
-      } else {
-        "LR_EDS21"
-      }
-    }
-    SentenceTypeClassification.SOPC -> {
-      if (sentenceType.nomisSentenceCalcType == "SOPC18") {
-        "LR_SOPC18"
-      } else {
-        "LR_SOPC21"
-      }
-    }
-    else -> throw IllegalStateException("Unknown sentence type classification ${sentenceType.classification} with standard recall")
+    SentenceTypeClassification.STANDARD -> getStandardRecallType(sentenceType)
+    SentenceTypeClassification.EXTENDED -> getExtendedRecallType(sentenceType)
+    SentenceTypeClassification.SOPC -> getSopcExtendedRecallType(sentenceType)
+    SentenceTypeClassification.INDETERMINATE -> getIndeterminateRecallType(sentenceType)
+    else -> "LR"
+  }
+
+  private fun getIndeterminateRecallType(sentenceType: SentenceTypeEntity): String = when (sentenceType.nomisSentenceCalcType) {
+    "ALP" -> "LR_ALP"
+    "ALP_CODE18" -> "LR_ALP_CDE18"
+    "ALP_CODE21" -> "LR_ALP_CDE21"
+    "ALP_LASPO" -> "LR_ALP_LASPO"
+    "DLP" -> "LR_DLP"
+    "IPP" -> "LR_IPP"
+    "MLP" -> "LR_MLP"
+    else -> "LR_LIFE"
+  }
+
+  private fun getSopcExtendedRecallType(sentenceType: SentenceTypeEntity): String = when (sentenceType.nomisSentenceCalcType) {
+    "SEC236A" -> "LR_SEC236A"
+    "SOPC18" -> "LR_SOPC18"
+    "SOPC21" -> "LR_SOPC21"
+    "SDOPCU18" -> "LR_SOPC18"
+    else -> "LR_SOPC21"
+  }
+
+  private fun getExtendedRecallType(sentenceType: SentenceTypeEntity): String = when (sentenceType.nomisSentenceCalcType) {
+    "LASPO_AR" -> "LR_LASPO_AR"
+    "LASPO_DR" -> "LR_LASPO_DR"
+    "EDS18" -> "LR_EDS18"
+    "EDS21" -> "LR_EDS21"
+    "EDSU18" -> "LR_EDSU18"
+    "EPP" -> "LR_EPP"
+    "EXT" -> "LR"
+    "STS18" -> "LR"
+    "STS21" -> "LR"
+    else -> "LR_EDS21"
+  }
+
+  private fun getStandardRecallType(sentenceType: SentenceTypeEntity): String = when (sentenceType.nomisSentenceCalcType) {
+    "ADIMP" -> "LR"
+    "ADIMP_ORA" -> "LR_ORA"
+    "SEC250" -> "LRSEC250_ORA"
+    "SEC250_ORA" -> "LRSEC250_ORA"
+    "YOI" -> "LR"
+    "YOI_ORA" -> "LR_YOI_ORA"
+    else -> "LR"
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/legacy/sentence/LegacyGetSentenceSentenceTypeTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/legacy/sentence/LegacyGetSentenceSentenceTypeTests.kt
@@ -16,40 +16,123 @@ class LegacyGetSentenceSentenceTypeTests {
 
   @ParameterizedTest(name = "DPS to NOMIS sentence type, original sentence type {0} and classification {1} recall type {2} will result in legacy recall sentence type {3}")
   @MethodSource("nomisToDpsSentenceTypeParameters")
-  fun `recall sentence type mappings`(dpsSentenceType: String, classification: SentenceTypeClassification, recallType: RecallType, expectedLegacySentenceType: String) {
-    val sentenceType = SentenceTypeEntity(sentenceTypeUuid = UUID.randomUUID(), description = classification.name, minAgeInclusive = null, maxAgeExclusive = null, minDateInclusive = null, maxDateExclusive = null, minOffenceDateInclusive = null, maxOffenceDateExclusive = null, classification = classification, hintText = null, nomisCjaCode = "", nomisSentenceCalcType = dpsSentenceType, displayOrder = 1, status = ReferenceEntityStatus.ACTIVE)
+  fun `recall sentence type mappings`(
+    nomisCjaCode: String,
+    nomisSentenceCalcType: String,
+    classification: SentenceTypeClassification,
+    recallType: RecallType,
+    expectedLegacySentenceType: String,
+    expectedCategory: String,
+  ) {
+    val sentenceType = SentenceTypeEntity(
+      sentenceTypeUuid = UUID.randomUUID(),
+      description = classification.name,
+      minAgeInclusive = null,
+      maxAgeExclusive = null,
+      minDateInclusive = null,
+      maxDateExclusive = null,
+      minOffenceDateInclusive = null,
+      maxOffenceDateExclusive = null,
+      classification = classification,
+      hintText = null,
+      nomisCjaCode = nomisCjaCode,
+      nomisSentenceCalcType = nomisSentenceCalcType,
+      displayOrder = 1,
+      status = ReferenceEntityStatus.ACTIVE,
+    )
 
     val legacySentenceType = RecallTypeEntity(1, recallType, recallType.name).toLegacySentenceType(sentenceType)
 
     assertThat(legacySentenceType.first).isEqualTo(expectedLegacySentenceType)
+    assertThat(legacySentenceType.second).isEqualTo(expectedCategory)
   }
 
   companion object {
     @JvmStatic
     fun nomisToDpsSentenceTypeParameters(): Stream<Arguments> = Stream.of(
-      // Standard sentence recall types
-      Arguments.of("ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.FTR_14, "14FTR_ORA"),
-      Arguments.of("ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.FTR_28, "FTR"),
-      Arguments.of("ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.FTR_HDC_14, "14FTRHDC_ORA"),
-      Arguments.of("ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.FTR_HDC_28, "FTR_HDC"),
-      Arguments.of("ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.CUR_HDC, "CUR"),
-      Arguments.of("ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.IN_HDC, "HDR"),
-      Arguments.of("ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.LR_HDC, "LR"),
-      Arguments.of("ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.LR, "LR"),
+      // Non-standard recalls
+      Arguments.of("2020", "ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.FTR_14, "14FTR_ORA", "2020"),
+      Arguments.of("2020", "ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.FTR_28, "FTR_ORA", "2020"),
+      Arguments.of("2020", "ADIMP", SentenceTypeClassification.STANDARD, RecallType.FTR_28, "FTR", "2020"),
+      Arguments.of(
+        "2020",
+        "ADIMP_ORA",
+        SentenceTypeClassification.STANDARD,
+        RecallType.FTR_HDC_14,
+        "14FTRHDC_ORA",
+        "2020",
+      ),
+      Arguments.of("2020", "ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.FTR_HDC_28, "FTR_HDC", "2020"),
+      Arguments.of("2020", "ADIMP", SentenceTypeClassification.STANDARD, RecallType.FTR_HDC_28, "FTR_HDC", "2020"),
+      Arguments.of("2020", "ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.CUR_HDC, "CUR_ORA", "2020"),
+      Arguments.of("2020", "ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.IN_HDC, "HDR_ORA", "2020"),
+      Arguments.of("2020", "ADIMP", SentenceTypeClassification.STANDARD, RecallType.CUR_HDC, "CUR", "2020"),
+      Arguments.of("2020", "ADIMP", SentenceTypeClassification.STANDARD, RecallType.IN_HDC, "HDR", "2020"),
+      Arguments.of("2020", "ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.LR_HDC, "LR", "2020"),
 
-      // Extended sentence recall types
-      Arguments.of("EDS18", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EDS21"),
-      Arguments.of("EDS21", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EDS21"),
-      Arguments.of("EDSU18", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EDS21"),
-      Arguments.of("EPP", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EDS21"),
-      Arguments.of("LASPO_AR", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_LASPO_AR"),
-      Arguments.of("LASPO_DR", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_LASPO_DR"),
-      Arguments.of("STS18", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EDS21"),
-      Arguments.of("STS21", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EDS21"),
+      // Standard recalls
+      // Standard sentence types
+      Arguments.of("2020", "ADIMP_ORA", SentenceTypeClassification.STANDARD, RecallType.LR, "LR_ORA", "2020"),
+      Arguments.of("2020", "ADIMP", SentenceTypeClassification.STANDARD, RecallType.LR, "LR", "2020"),
+      Arguments.of("2020", "SEC250", SentenceTypeClassification.STANDARD, RecallType.LR, "LRSEC250_ORA", "2020"),
+      Arguments.of("2020", "SEC250_ORA", SentenceTypeClassification.STANDARD, RecallType.LR, "LRSEC250_ORA", "2020"),
+      Arguments.of("2020", "YOI", SentenceTypeClassification.STANDARD, RecallType.LR, "LR", "2020"),
+      Arguments.of("2020", "YOI_ORA", SentenceTypeClassification.STANDARD, RecallType.LR, "LR_YOI_ORA", "2020"),
+      Arguments.of("2003", "YOI", SentenceTypeClassification.STANDARD, RecallType.LR, "LR", "2003"),
+      Arguments.of("2003", "YOI_ORA", SentenceTypeClassification.STANDARD, RecallType.LR, "LR_YOI_ORA", "2003"),
 
-      // SOPC recall types
-      Arguments.of("SOPC18", SentenceTypeClassification.SOPC, RecallType.LR, "LR_SOPC18"),
-      Arguments.of("SOPC21", SentenceTypeClassification.SOPC, RecallType.LR, "LR_SOPC21"),
+      // Extended sentence types
+      Arguments.of("2020", "EDS18", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EDS18", "2020"),
+      Arguments.of("2020", "EDS21", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EDS21", "2020"),
+      Arguments.of("2020", "EDSU18", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EDSU18", "2020"),
+      Arguments.of("2003", "EPP", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_EPP", "2003"),
+      Arguments.of("2003", "LASPO_AR", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_LASPO_AR", "2003"),
+      Arguments.of("2003", "LASPO_DR", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR_LASPO_DR", "2003"),
+      Arguments.of("2020", "STS18", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR", "2020"),
+      Arguments.of("2020", "STS21", SentenceTypeClassification.EXTENDED, RecallType.LR, "LR", "2020"),
+
+      // SOPC sentence types
+      Arguments.of("2020", "SOPC18", SentenceTypeClassification.SOPC, RecallType.LR, "LR_SOPC18", "2020"),
+      Arguments.of("2020", "SOPC21", SentenceTypeClassification.SOPC, RecallType.LR, "LR_SOPC21", "2020"),
+      Arguments.of("2003", "SEC236A", SentenceTypeClassification.SOPC, RecallType.LR, "LR_SEC236A", "2003"),
+      Arguments.of("2020", "SDOPCU18", SentenceTypeClassification.SOPC, RecallType.LR, "LR_SOPC18", "2020"),
+
+      // Indeterminate sentence types.
+      Arguments.of("2020", "ALP", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_ALP", "2020"),
+      Arguments.of(
+        "2020",
+        "ALP_CODE18",
+        SentenceTypeClassification.INDETERMINATE,
+        RecallType.LR,
+        "LR_ALP_CDE18",
+        "2020",
+      ),
+      Arguments.of(
+        "2020",
+        "ALP_CODE21",
+        SentenceTypeClassification.INDETERMINATE,
+        RecallType.LR,
+        "LR_ALP_CDE21",
+        "2020",
+      ),
+      Arguments.of(
+        "2003",
+        "ALP_LASPO",
+        SentenceTypeClassification.INDETERMINATE,
+        RecallType.LR,
+        "LR_ALP_LASPO",
+        "2003",
+      ),
+      Arguments.of("2020", "DFL", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_LIFE", "2020"),
+      Arguments.of("2020", "DLP", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_DLP", "2020"),
+      Arguments.of("2003", "IPP", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_IPP", "2003"),
+      Arguments.of("1991", "LIFE", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_LIFE", "1991"),
+      Arguments.of("2020", "MLP", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_MLP", "2020"),
+      Arguments.of("2020", "SEC272", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_LIFE", "2020"),
+      Arguments.of("2020", "SEC275", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_LIFE", "2020"),
+      Arguments.of("1991", "SEC93", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_LIFE", "1991"),
+      Arguments.of("2003", "SEC93_03", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_LIFE", "2003"),
+      Arguments.of("2003", "SEC94", SentenceTypeClassification.INDETERMINATE, RecallType.LR, "LR_LIFE", "2003"),
     )
   }
 }
