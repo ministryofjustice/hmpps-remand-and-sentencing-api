c8ec4d4ecafd52f50971c8ea59efff33f2762b2d
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-06-02 16:04:15 +0100

    RASS-1127 initial commit of native query for search endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/projection/CourtCaseRow.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/projection/CourtCaseRow.kt
@@ -1,7 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.projection
 
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.EntityStatus
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.PeriodLengthType
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.legacy.controller.dto.CourtAppearanceLegacyData
@@ -13,24 +11,26 @@ data class CourtCaseRow(
   var courtCaseId: Int,
   var prisonerId: String,
   var courtCaseUuid: String,
-  @Enumerated(EnumType.ORDINAL)
   var courtCaseStatus: EntityStatus,
   var courtCaseLegacyData: CourtCaseLegacyData?,
   var appearanceCount: Long,
   var caseReferences: String,
   var firstDayInCustody: LocalDate,
-  var latestCourtAppearanceWarrantType: String,
-  var dpsOverallCourtCaseOutcome: String?,
-  var latestCourtAppearanceLegacyData: CourtAppearanceLegacyData?,
   var appearancePeriodLengthYears: Int?,
   var appearancePeriodLengthMonths: Int?,
   var appearancePeriodLengthWeeks: Int?,
   var appearancePeriodLengthDays: Int?,
   var appearancePeriodLengthOrder: String?,
   var appearancePeriodLengthType: PeriodLengthType?,
-  var latestCourtAppearanceOverallConvictionDate: LocalDate?,
   var nextCourtAppearanceCourtCode: String?,
   var nextCourtAppearanceTypeDescription: String?,
   var nextCourtAppearanceDate: LocalDate?,
   var nextCourtAppearanceTime: LocalTime?,
+  var latestCourtAppearanceCaseReference: String?,
+  var latestCourtAppearanceCourtCode: String,
+  var latestCourtAppearanceDate: LocalDate,
+  var latestCourtAppearanceWarrantType: String,
+  var latestCourtAppearanceOutcome: String?,
+  var latestCourtAppearanceLegacyData: CourtAppearanceLegacyData?,
+  var latestCourtAppearanceOverallConvictionDate: LocalDate?,
 )
