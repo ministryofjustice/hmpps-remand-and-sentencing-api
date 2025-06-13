25d43025cb25e440a0890fc828cf6d2dc4c6d111
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-06-13 10:00:16 +0100

    RCLL-386 Delete sentence if legacy recall.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/recall/RecallIntTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/recall/RecallIntTests.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.CreateRecall
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.Recall
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.legacy.util.DataCreator
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.RecallType.FTR_14
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.RecallType.FTR_28
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.RecallType.LR
@@ -383,5 +384,25 @@ class RecallIntTests : IntegrationTestBase() {
     assertThat(messages).hasSize(1)
       .extracting<String> { it.eventType }.contains("recall.deleted")
     assertThat(messages[0].additionalInformation.get("previousRecallId").asText()).isEqualTo(recallOneId.toString())
+  }
+
+  @Test
+  fun `Delete a legacy recall should also delete sentence`() {
+    // Create a legacy sentence so that the legacy recall is also created.
+    val (lifetimeUuid, createdSentence) = createLegacySentence(
+      legacySentence = DataCreator.legacyCreateSentence(sentenceLegacyData = DataCreator.sentenceLegacyData(sentenceCalcType = "FTR_ORA", sentenceCategory = "2020"), returnToCustodyDate = LocalDate.of(2023, 1, 1)),
+    )
+    val recall = getRecallsByPrisonerId(DpsDataCreator.DEFAULT_PRISONER_ID).first()
+    purgeQueues()
+
+    deleteRecall(recall.recallUuid)
+
+    val recalls = getRecallsByPrisonerId(DpsDataCreator.DEFAULT_PRISONER_ID)
+
+    assertThat(recalls).isEmpty()
+
+    val messages = getMessages(2)
+    assertThat(messages).hasSize(2)
+      .extracting<String> { it.eventType }.contains("recall.deleted", "sentence.deleted")
   }
 }
