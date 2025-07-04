feced1d7891e9c707ccdb2fb0ae819793dc826f3
Yasin Mustafa <yasin.mustafa@digital.justice.gov.uk>
2025-07-04 09:18:23 +0100

    RASS-1162 Add end point to get latest offence date for a court case (#555)
    * Add end point to get latest offence date for a court case
    
    * Optimise queries so only one query runs
    
    * revert temp properties change
    
    * Code tidy
    
    * Change role for endpoint to be ROLE_REMAND_AND_SENTENCING__REMAND_AND_SENTENCING_UI
    
    * Change role for endpoint to be ROLE_REMAND_AND_SENTENCING__REMAND_AND_SENTENCING_UI

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/courtcase/GetCourtCaseLatestOffenceDateTests.kt
@@ -0,0 +1,59 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.courtcase
+
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.util.DpsDataCreator
+import java.time.LocalDate
+
+class GetCourtCaseLatestOffenceDateTests : IntegrationTestBase() {
+
+  @Test
+  fun `should return latest offence date for court case`() {
+    val offenceStart = LocalDate.now().minusDays(10)
+    val offenceEnd = LocalDate.now().minusDays(5)
+    val charge = DpsDataCreator.dpsCreateCharge(offenceStartDate = offenceStart, offenceEndDate = offenceEnd)
+    val appearance = DpsDataCreator.dpsCreateCourtAppearance(charges = listOf(charge))
+    val courtCase = DpsDataCreator.dpsCreateCourtCase(appearances = listOf(appearance))
+    val (courtCaseUuid) = createCourtCase(courtCase)
+
+    webTestClient.get()
+      .uri("/court-case/$courtCaseUuid/latest-offence-date")
+      .headers { it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING__REMAND_AND_SENTENCING_UI")) }
+      .exchange()
+      .expectStatus().isOk
+      .expectBody(LocalDate::class.java)
+      .isEqualTo(offenceEnd)
+  }
+
+  @Test
+  fun `should return 204 when no valid offence dates exist`() {
+    val appearance = DpsDataCreator.dpsCreateCourtAppearance(charges = emptyList())
+    val courtCase = DpsDataCreator.dpsCreateCourtCase(appearances = listOf(appearance))
+    val (courtCaseUuid) = createCourtCase(courtCase)
+
+    webTestClient.get()
+      .uri("/court-case/$courtCaseUuid/latest-offence-date")
+      .headers { it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING__REMAND_AND_SENTENCING_UI")) }
+      .exchange()
+      .expectStatus().isNoContent
+  }
+
+  @Test
+  fun `no token results in unauthorized`() {
+    val (courtCaseUuid) = createCourtCase()
+    webTestClient.get()
+      .uri("/court-case/$courtCaseUuid/latest-offence-date")
+      .exchange()
+      .expectStatus().isUnauthorized
+  }
+
+  @Test
+  fun `token with incorrect role is forbidden`() {
+    val (courtCaseUuid) = createCourtCase()
+    webTestClient.get()
+      .uri("/court-case/$courtCaseUuid/latest-offence-date")
+      .headers { it.authToken(roles = listOf("ROLE_OTHER_FUNCTION")) }
+      .exchange()
+      .expectStatus().isForbidden
+  }
+}
