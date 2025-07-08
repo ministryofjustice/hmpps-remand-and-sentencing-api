2dcbd14c1ec57a512e5d5afe1e9290a2fda0bba5
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-07-08 08:29:02 +0100

    add tiscs to legacy sentence types

---
+++ src/main/resources/migration/common/V102__add_tiscs_legacy_sentence_type.sql
@@ -0,0 +1,4 @@
+INSERT INTO legacy_sentence_types (
+    nomis_reference, classification, sentencing_act, nomis_active, nomis_expiry_date, nomis_description, recall_type, eligibility, nomis_terms, sentence_type_uuid
+) VALUES
+('TISCS','',2020, TRUE, '', 'Term of Imprisonment, Intensive Supervision Court Sanction - Schedule 10, Sentencing Act 2020','', NULL, '["IMP"]'::jsonb, NULL);
