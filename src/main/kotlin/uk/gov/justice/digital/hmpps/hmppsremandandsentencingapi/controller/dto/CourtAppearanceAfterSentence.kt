7d8c04c7fbd38e10af683a52e2f2ec96cfd3ac54
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-07-10 09:29:47 +0100

    RASS-1182 add sentence after on other court appearance details endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/CourtAppearanceAfterSentence.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/CourtAppearanceAfterSentence.kt
@@ -2,14 +2,17 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto
 
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.projection.SentenceAfterOnAnotherCourtAppearanceRow
 import java.time.LocalDate
+import java.util.UUID
 
 data class CourtAppearanceAfterSentence(
+  val appearanceUuid: UUID,
   val caseReference: String?,
   val appearanceDate: LocalDate,
   val courtCode: String,
 ) {
   companion object {
     fun from(sentenceAfterOnAnotherCourtAppearanceRow: SentenceAfterOnAnotherCourtAppearanceRow): CourtAppearanceAfterSentence = CourtAppearanceAfterSentence(
+      sentenceAfterOnAnotherCourtAppearanceRow.appearanceUuid,
       sentenceAfterOnAnotherCourtAppearanceRow.caseReference,
       sentenceAfterOnAnotherCourtAppearanceRow.appearanceDate,
       sentenceAfterOnAnotherCourtAppearanceRow.courtCode,
