1977c6975f710b1abbc5871c03357b38cb6f77f6
Yasin Mustafa <yasin.mustafa@digital.justice.gov.uk>
2025-08-12 16:19:22 +0100

    RASS-1209 Add MergedToCaseDetails to CourtCase DTO (if a merged case exists) (#605)
    * RASS-1209 AddMergedToCaseDetails to CourtCase DTO object
    
    * Add int test to check that a merged case details are returned
    
    * lint fix
    
    * Only set merged details if case is active and latest appearance is active and exists
    
    * lint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/CourtCase.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/CourtCase.kt
@@ -11,6 +11,7 @@ data class CourtCase(
   val latestAppearance: CourtAppearance?,
   val appearances: List<CourtAppearance>,
   val legacyData: CourtCaseLegacyData?,
+  val mergedToCaseDetails: MergedToCaseDetails?,
 ) {
   companion object {
     fun from(courtCaseEntity: CourtCaseEntity): CourtCase = CourtCase(
@@ -20,6 +21,17 @@ data class CourtCase(
       courtCaseEntity.latestCourtAppearance?.let { CourtAppearance.from(it) },
       courtCaseEntity.appearances.filter { it.statusId == EntityStatus.ACTIVE }.map { CourtAppearance.from(it) },
       courtCaseEntity.legacyData,
+      mergedToCaseDetails = courtCaseEntity.mergedToCase
+        ?.takeIf { it.statusId == EntityStatus.ACTIVE && it.latestCourtAppearance?.statusId == EntityStatus.ACTIVE }
+        ?.let { mergedTo ->
+          val latestAppearance = mergedTo.latestCourtAppearance
+          MergedToCaseDetails(
+            mergedToDate = courtCaseEntity.mergedToDate,
+            caseReference = mergedTo.latestCourtAppearance?.courtCaseReference,
+            courtCode = latestAppearance?.courtCode,
+            warrantDate = latestAppearance?.appearanceDate,
+          )
+        },
     )
   }
 }
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/MergedToCaseDetails.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto
+
+import java.time.LocalDate
+
+data class MergedToCaseDetails(
+  val mergedToDate: LocalDate?,
+  val caseReference: String?,
+  val courtCode: String?,
+  val warrantDate: LocalDate?,
+)
