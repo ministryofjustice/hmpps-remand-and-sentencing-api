4707f761f6502570bff16e773316256546f85ebe
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2025-09-18 11:30:15 +0100

    RASS-1349 process move booking event (#662)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/PrisonerEventService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/PrisonerEventService.kt
@@ -3,12 +3,25 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.service
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.audit.CourtCaseHistoryEntity
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.repository.CourtCaseRepository
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.repository.audit.CourtCaseHistoryRepository
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.listener.dto.PrisonerBookingMovedEvent
+import java.time.ZonedDateTime
 
 @Service
-class PrisonerEventService {
+class PrisonerEventService(private val courtCaseRepository: CourtCaseRepository, private val courtCaseHistoryRepository: CourtCaseHistoryRepository) {
+  @Transactional
   fun handleBookingMoved(event: PrisonerBookingMovedEvent) {
     log.info("handling booking moved from {} to {}", event.additionalInformation.movedFromNomsNumber, event.additionalInformation.movedToNomsNumber)
+    val courtCases = courtCaseRepository.findByPrisonerIdAndBookingId(event.additionalInformation.movedFromNomsNumber, event.additionalInformation.bookingId)
+    courtCases.forEach { courtCase ->
+      courtCase.prisonerId = event.additionalInformation.movedToNomsNumber
+      courtCase.updatedAt = ZonedDateTime.now()
+      courtCase.updatedBy = "NOMIS"
+      courtCaseHistoryRepository.save(CourtCaseHistoryEntity.from(courtCase))
+    }
   }
 
   private companion object {
