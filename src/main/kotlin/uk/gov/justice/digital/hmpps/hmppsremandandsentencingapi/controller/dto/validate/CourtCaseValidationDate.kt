976eaef341435953d2d6596579461b222d2f604a
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-10-16 11:23:38 +0100

    RASS-1373 add endpoint to return max offence date and max remand date

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/validate/CourtCaseValidationDate.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.validate
+
+import java.time.LocalDate
+
+data class CourtCaseValidationDate(
+  val offenceDate: LocalDate?,
+  val latestRemandAppearanceDate: LocalDate?,
+)
