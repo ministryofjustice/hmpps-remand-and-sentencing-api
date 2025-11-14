c039657ce0047e440c61e2a0bd4ca51b913c95d1
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-11-14 13:03:53 +0000

    RASS-1649 add cache for sentence type call

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/config/CacheConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/config/CacheConfiguration.kt
@@ -1,24 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.config
 
-import org.slf4j.LoggerFactory
-import org.springframework.boot.CommandLineRunner
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.legacy.service.LegacySentenceTypesService
 
 @EnableCaching
 @Configuration
-class CacheConfiguration {
-
-  @Bean
-  fun fillLegacySentenceTypeCache(legacySentenceTypesService: LegacySentenceTypesService): CommandLineRunner = CommandLineRunner {
-    log.info("Calling fillLegacySentenceTypeCache")
-    val allLegacySentenceTypes = legacySentenceTypesService.getAllLegacySentences()
-    allLegacySentenceTypes.map { it.nomisSentenceTypeReference }.distinct().forEach { legacySentenceTypesService.getLegacySentencesByNomisSentenceTypeReferenceAsSummary(it) }
-  }
-
-  companion object {
-    private val log = LoggerFactory.getLogger(this::class.java)
-  }
-}
+class CacheConfiguration
