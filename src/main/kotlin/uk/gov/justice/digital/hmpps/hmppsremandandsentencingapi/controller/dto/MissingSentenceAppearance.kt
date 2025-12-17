11a3b700d5e51468006af0a69e82bba3cd6ca758
Md Hasan Rahman <hassan.rahman@justice.gov.uk>
2025-12-17 14:30:17 +0000

    Rass 1701 endpoint for sentence with type unknown recall (#824)
    * RASS-1701-endpoint added for getting sentences with unknown recall type
    
    * small refactoring
    
    * small refactoring
    
    * small refactoring
    
    * RASS-1701-custom response model added for getting sentence with unknown recall type

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/MissingSentenceAppearance.kt
@@ -0,0 +1,31 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto
+
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.projection.MissingSentenceInformationDetails
+import java.time.LocalDate
+
+data class MissingSentenceAppearance(
+  val appearanceUuid: String,
+  val courtCode: String,
+  val courtCaseReference: String?,
+  val appearanceDate: LocalDate,
+  val sentences: List<SentenceCardDetails>,
+) {
+  companion object {
+    fun from(
+      details: List<MissingSentenceInformationDetails>,
+    ): List<MissingSentenceAppearance> = details
+      .groupBy { it.appearanceUuid }
+      .values
+      .map { appearanceDetails ->
+        val first = appearanceDetails.first()
+
+        MissingSentenceAppearance(
+          appearanceUuid = first.appearanceUuid.toString(),
+          courtCode = first.courtCode,
+          courtCaseReference = first.courtCaseReference,
+          appearanceDate = first.appearanceDate.toLocalDate(),
+          sentences = appearanceDetails.map(SentenceCardDetails::from),
+        )
+      }
+  }
+}
