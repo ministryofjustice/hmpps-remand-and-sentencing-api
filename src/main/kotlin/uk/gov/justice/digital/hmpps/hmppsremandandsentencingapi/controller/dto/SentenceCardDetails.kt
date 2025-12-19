231a1f8598fba16efc3d9c244ec34af277134bed
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2025-12-19 14:04:46 +0000

    RASS-1701-adding charge id and group by sentence

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/SentenceCardDetails.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/SentenceCardDetails.kt
@@ -9,6 +9,7 @@ data class SentenceCardDetails(
   val offenceCode: String,
   val offenceStartDate: LocalDate?,
   val sentenceUuid: UUID,
+  val chargeUuid: UUID,
   val countNumber: String?,
   val sentenceType: String,
   val periodLengths: List<PeriodLength>,
@@ -37,11 +38,42 @@ data class SentenceCardDetails(
         offenceCode = missingSentenceInformationDetails.offenceCode,
         offenceStartDate = missingSentenceInformationDetails.offenceStartDate?.toLocalDate(),
         sentenceUuid = missingSentenceInformationDetails.sentenceUuid,
+        chargeUuid = missingSentenceInformationDetails.chargeUuid,
         countNumber = missingSentenceInformationDetails.countNumber,
         sentenceType = missingSentenceInformationDetails.sentenceTypeDescription,
         periodLengths = periodLengths,
         sentenceServeType = missingSentenceInformationDetails.sentenceServeType,
         convictedDate = missingSentenceInformationDetails.convictionDate?.toLocalDate(),
+      )
+    }
+    fun fromList(sentenceRows: List<MissingSentenceInformationDetails>): SentenceCardDetails {
+      val first = sentenceRows.first()
+
+      val periodLengths = sentenceRows
+        .filter { it.periodLengthUuid != null }
+        .map { row ->
+          PeriodLength(
+            years = row.years,
+            months = row.months,
+            weeks = row.weeks,
+            days = row.days,
+            periodOrder = row.periodOrder!!,
+            periodLengthType = PeriodLengthType.valueOf(row.periodLengthType!!),
+            legacyData = null,
+            periodLengthUuid = row.periodLengthUuid!!,
+          )
+        }
+
+      return SentenceCardDetails(
+        offenceCode = first.offenceCode,
+        offenceStartDate = first.offenceStartDate?.toLocalDate(),
+        sentenceUuid = first.sentenceUuid,
+        chargeUuid = first.chargeUuid,
+        countNumber = first.countNumber,
+        sentenceType = first.sentenceTypeDescription,
+        periodLengths = periodLengths, // This is now the full list
+        sentenceServeType = first.sentenceServeType,
+        convictedDate = first.convictionDate?.toLocalDate(),
       )
     }
   }
