4f77eeaeb75637ac17ec0a7523ef8781986fa212
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-01-08 13:37:46 +0000

    RASS-1749 expose uuids to ui so that audit can take place

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/paged/PagedMergedFromCase.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/paged/PagedMergedFromCase.kt
@@ -2,8 +2,10 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.
 
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.projection.CourtCaseRow
 import java.time.LocalDate
+import java.util.UUID
 
 data class PagedMergedFromCase(
+  val appearanceUuid: UUID,
   val caseReference: String?,
   val courtCode: String,
   val warrantDate: LocalDate,
@@ -13,6 +15,7 @@ data class PagedMergedFromCase(
     fun from(courtCaseRows: List<CourtCaseRow>): PagedMergedFromCase {
       val mergedFromCase = courtCaseRows.first()
       return PagedMergedFromCase(
+        mergedFromCase.mergedFromAppearanceUuid!!,
         mergedFromCase.mergedFromCaseReference,
         mergedFromCase.mergedFromCourtCode!!,
         mergedFromCase.mergedFromWarrantDate!!,
