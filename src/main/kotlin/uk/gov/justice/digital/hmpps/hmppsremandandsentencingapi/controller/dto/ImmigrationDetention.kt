4cdec6133608970b69aee9dffeda641b4c50825e
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2026-01-16 12:18:58 +0000

    RASS-1772 add bail immigration record type (#862)
    * RASS-1772 add bail immigration record type
    
    * RASS-1772 add bail immigration record type

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/ImmigrationDetention.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/ImmigrationDetention.kt
@@ -42,7 +42,9 @@ data class ImmigrationDetention(
 
     private fun getImmigrationDetentionTypeFromNOMIS(code: String?): ImmigrationDetentionRecordType = when (code) {
       "5500" -> ImmigrationDetentionRecordType.IS91
+      "5501" -> ImmigrationDetentionRecordType.IMMIGRATION_BAIL
       "5502" -> ImmigrationDetentionRecordType.DEPORTATION_ORDER
+      "5503" -> ImmigrationDetentionRecordType.NO_LONGER_OF_INTEREST
       else -> ImmigrationDetentionRecordType.UNKNOWN
     }
 
