21bcd120fa3d6e66f4d429b7c2db90e410ab409c
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-02-05 10:49:17 +0000

    RASS-1870 RASS-1871 remove migration from flyway (took too long), have to run manually

--- src/main/resources/migration/common/V144__migrate_suspended_sentence_implemented_outcomes.sql
+++
@@ -1,5 +0,0 @@
-with appearanceOutcome AS (select id from appearance_outcome ao where ao.outcome_uuid = 'c3d9a1e6-2b74-4f5a-9d3c-7e1f0a8b6c54')
-update court_appearance set appearance_outcome_id = appearanceOutcome.id from appearanceOutcome where legacy_data ->> 'nomisOutcomeCode' = '1507';
-
-with chargeOutcome as (select id from charge_outcome co where co.outcome_uuid = '8a5c4f7d-8e9c-4c1b-9f8e-2f4a3a6d0b21')
-update charge set charge_outcome_id = chargeOutcome.id from chargeOutcome where legacy_data ->> 'nomisOutcomeCode' = '1507';
