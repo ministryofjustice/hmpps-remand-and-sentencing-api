2035d4c82a6f87599a091b3c342d3488e35f849d
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2026-02-25 09:10:53 +0000

    RASS-1766 add endpoint to generate failed telemetry event (#936)
    * RASS-1766 add endpoint to generate failed telemetry event
    
    * RASS-1766 add endpoint to generate failed telemetry event

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/TempController.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.microsoft.applicationinsights.TelemetryClient
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.domain.event.HmppsCourtCaseMessage
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.domain.event.PersonReference
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.domain.event.PersonReferenceType
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.repository.CourtCaseRepository
+
+@RestController
+@RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+class TempController(private val telemetryClient: TelemetryClient, private val objectMapper: ObjectMapper, private val courtCaseRepository: CourtCaseRepository) {
+
+  @PostMapping("/temp/generate-failed-court-case-event-publish-telemetry")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  fun generateFailedEventPublishTelemetry() {
+    courtCaseRepository.findFirstByStatusId()?.let { activeCourtCase ->
+      telemetryClient.trackEvent("failed-domain-event-published", mapOf("eventType" to "court-case.updated", "description" to "Court case updated", "additionalInformation" to objectMapper.writeValueAsString(HmppsCourtCaseMessage(activeCourtCase.caseUniqueIdentifier)), "personReference" to objectMapper.writeValueAsString(PersonReference(listOf(PersonReferenceType("NOMS", activeCourtCase.prisonerId))))), null)
+    }
+  }
+}
