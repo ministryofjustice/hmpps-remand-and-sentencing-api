039f496a7af7ae8cb16db15bddc7485c272ca55f
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-04-17 16:08:26 +0100

    RASS-2095 populate more charge outcome sentence type combos

---
+++ src/main/resources/migration/common/V157__insert_charge_outcome_sentence_type_records.sql
@@ -0,0 +1,156 @@
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='18d5af6d-2fa7-4166-a4c9-8381a1e3c7e0')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='f1ddc9d6-3c2e-419a-b54d-4e05c92a0f55')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='b14be497-e9fe-4b67-913b-91b6c27a57c4')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='1104e683-5467-4340-b961-ff53672c4f39')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='f1fb11de-aa51-4a74-8c5c-9c08ada6db37')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='6ad464cc-08d9-438c-85c4-3352c25756c4')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='d34c90fe-cc49-4ac0-8ead-ca3fcc261b24')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='5fc63cba-510f-4122-8fdd-649fbcd52efb')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='4d434819-4aff-442a-8c78-64dfcfba61cc')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='4eaf630e-1629-43a0-aaef-31dfd805e54a')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='143c501e-b4dd-4709-8cef-7e06168fc6db')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='e5a4cfc1-7b7d-4cbe-9e7f-863decbf3787')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='a12c98e1-7321-4c41-9684-6a79cd789af8')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='8f23e11b-dfa6-4d97-81ca-07ccd5f9983a')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='69811a64-c18a-4e18-9f9a-87a3fa7ee0bd')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='d8d38763-bee6-474e-8988-7dfa3d02f3ae')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='3725c368-fbfb-4e46-babf-12aabc2a7f91')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='80584016-4bab-4408-a8f2-b142830819e5')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='b7b6a775-6146-484f-a8a6-92f2ed8fa6ae')
+    from charge_outcome co
+    where co.outcome_uuid='3a1d8f72-7b2e-4f5c-8d4a-6c9e1b7f2d03';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='e8098305-f909-4cf5-9b06-f71a4e4d9df0')
+    from charge_outcome co
+    where co.outcome_uuid='0460ad51-04ea-402a-a249-b152b052a385';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='cab9e914-e0de-48d0-9e72-0e1fc9a19cf4')
+    from charge_outcome co
+    where co.outcome_uuid='0460ad51-04ea-402a-a249-b152b052a385';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='b6862370-e8f0-4680-8797-0aca9cacb302')
+    from charge_outcome co
+    where co.outcome_uuid='0460ad51-04ea-402a-a249-b152b052a385';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='903ca33b-e264-4a16-883d-fee03a2a3396')
+    from charge_outcome co
+    where co.outcome_uuid='0460ad51-04ea-402a-a249-b152b052a385';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='c71ceefe-932b-4a69-b87c-7c1294e37cf7')
+    from charge_outcome co
+    where co.outcome_uuid='cc7efcbd-88a1-4cfd-99aa-8e8eacc8e8cb';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='4f8c2a67-3b5d-4a9f-bd2a-7d0e39c8c432')
+    from charge_outcome co
+    where co.outcome_uuid='cc7efcbd-88a1-4cfd-99aa-8e8eacc8e8cb';
+insert into charge_outcome_sentence_type(charge_outcome_id, sentence_type_id)
+    select id, (select id
+               from sentence_type st
+               where st.sentence_type_uuid='b7e1f0a3-9c24-4db8-8f62-5a1d0b71c9de')
+    from charge_outcome co
+    where co.outcome_uuid='cc7efcbd-88a1-4cfd-99aa-8e8eacc8e8cb';
