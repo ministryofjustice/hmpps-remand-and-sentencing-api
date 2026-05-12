dc3a694816103f22f21d2928ee623c2e720a2f59
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-05-12 11:27:35 +0100

    RASS-2138 fix compile issue and write test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/courtcase/GetCourtCaseSentencedChargesTests.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/integration/courtcase/GetCourtCaseSentencedChargesTests.kt
@@ -2,10 +2,34 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.cou
 
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.util.DpsDataCreator
+import java.util.UUID
 
 class GetCourtCaseSentencedChargesTests : IntegrationTestBase() {
 
   @Test
   fun `retrieve only the sentenced charges`() {
+    val remandedCharge = DpsDataCreator.dpsCreateCharge(sentence = null, outcomeUuid = UUID.fromString("315280e5-d53e-43b3-8ba6-44da25676ce2"))
+    val remandedCourtAppearance = DpsDataCreator.dpsCreateCourtAppearance(charges = listOf(remandedCharge), outcomeUuid = UUID.fromString("2f585681-7b1a-44fb-a0cb-f9a4b1d9cda8"))
+    val sentencedCharge = DpsDataCreator.dpsCreateCharge()
+    val sentencedCourtAppearance = DpsDataCreator.dpsCreateCourtAppearance(charges = listOf(sentencedCharge))
+    val remandedCourtCase = DpsDataCreator.dpsCreateCourtCase(appearances = listOf(remandedCourtAppearance, sentencedCourtAppearance))
+    val (courtCaseUuid) = createCourtCase(remandedCourtCase)
+    webTestClient
+      .get()
+      .uri("/court-case/$courtCaseUuid/sentenced-charges")
+      .headers {
+        it.authToken(roles = listOf("ROLE_REMAND_AND_SENTENCING__REMAND_AND_SENTENCING_UI"))
+      }
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("$.charges[?(@.chargeUuid == '${sentencedCharge.chargeUuid}')].offenceCode")
+      .isEqualTo(sentencedCharge.offenceCode)
+      .jsonPath("$.charges[?(@.chargeUuid == '${sentencedCharge.chargeUuid}')].outcome.outcomeUuid")
+      .isEqualTo(sentencedCharge.outcomeUuid.toString())
+      .jsonPath("$.charges[?(@.chargeUuid == '${remandedCharge.chargeUuid}')]")
+      .doesNotExist()
   }
 }
