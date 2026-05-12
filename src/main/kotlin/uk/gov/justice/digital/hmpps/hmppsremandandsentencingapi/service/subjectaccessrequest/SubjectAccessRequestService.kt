5c1e9f04f35b6d4babc7502cc30eeb7f47ec6b5e
David Maggio <david.maggio@justice.gov.uk>
2026-05-12 11:51:26 +0100

    Debug logging included for troubleshooting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/subjectaccessrequest/SubjectAccessRequestService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/service/subjectaccessrequest/SubjectAccessRequestService.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.service.subjectaccessrequest
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.config.ConditionalOnSarEnabled
 import uk.gov.justice.hmpps.kotlin.sar.HmppsPrisonSubjectAccessRequestService
@@ -17,6 +19,11 @@ class SubjectAccessRequestService(
     fromDate: LocalDate?,
     toDate: LocalDate?,
   ): HmppsSubjectAccessRequestContent? = dataPrisonerDetailsService.getPrisonerDetails(prn, fromDate, toDate)?.let {
+    log.debug("SAR Requested for {}", it.prisonerNumber)
     HmppsSubjectAccessRequestContent(it, listOf())
+  }
+
+  private companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
 }
