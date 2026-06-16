317f80d3745b98cf3716cd058d0813ad16b69198
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2026-06-16 14:54:19 +0100

    RASS-2321: Annotate `status` field in `AggravatingFactorEntity` with `@Enumerated(EnumType.STRING)` for enum mapping

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/AggravatingFactorEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/jpa/entity/AggravatingFactorEntity.kt
@@ -2,6 +2,8 @@ package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -27,6 +29,7 @@ class AggravatingFactorEntity(
   val description: String? = null,
 
   @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
   val status: AggravatingFactorStatus = AggravatingFactorStatus.ACTIVE,
 
   @Column(nullable = false)
