815367ada08feea26a16fd4084e37ed9140b32ad
Carlo Veo <144013256+carloveo-moj@users.noreply.github.com>
2026-06-22 09:07:48 +0100

    RASS-2330 add delete status endpoint (#1115)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/courtappearanceschedule/DeleteCourtAppearanceStatus.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto.courtappearanceschedule
+
+enum class DeleteCourtAppearanceStatus {
+  SUPPORTED,
+  NOT_SUPPORTED,
+}
