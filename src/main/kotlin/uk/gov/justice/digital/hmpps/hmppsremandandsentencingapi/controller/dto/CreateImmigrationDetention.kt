c133d7000f4ebb040b112634e68ac04a7b474a13
dmaggio-jd <david.maggio@justice.gov.uk>
2026-07-28 09:03:51 +0100

    Adding API validation to homeOfficeReferenceNumber

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/CreateImmigrationDetention.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/dto/CreateImmigrationDetention.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.controller.dto
 
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.ImmigrationDetentionNoLongerOfInterestType
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.enum.ImmigrationDetentionRecordType
 import java.time.LocalDate
@@ -10,6 +12,8 @@ data class CreateImmigrationDetention(
   val appearanceOutcomeUuid: UUID,
   var immigrationDetentionRecordType: ImmigrationDetentionRecordType,
   var recordDate: LocalDate,
+  @field:Size(min = 5, max = 16, message = "The Home Office Reference Number should be between 5 and 16 characters.")
+  @field:Pattern(regexp = "^[A-Za-z0-9/]+$", message = "The Home Office Reference Number should contain only letters, numbers and '/'")
   var homeOfficeReferenceNumber: String? = null,
   var noLongerOfInterestReason: ImmigrationDetentionNoLongerOfInterestType? = null,
   var noLongerOfInterestComment: String? = null,
