effe03c827a1a2460fb81e9b4edf9fb256fb81e7
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-08-10 11:44:53 +0100

    RASS-2546 add new period length type and ability to associate period length with appearance and sentence on create

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/domain/CourtCaseHierarchyData.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/domain/CourtCaseHierarchyData.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.domain
 
+import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.PeriodLengthEntity
 import uk.gov.justice.digital.hmpps.hmppsremandandsentencingapi.jpa.entity.SentenceTypeEntity
 import java.util.UUID
 
@@ -10,4 +11,5 @@ data class CourtCaseHierarchyData(
   var courtAppearanceDateChanged: Boolean? = false,
   var existingSentenceType: SentenceTypeEntity? = null,
   var updatedSentenceType: SentenceTypeEntity? = null,
+  val courtAppearancePeriodLengths: MutableSet<PeriodLengthEntity> = mutableSetOf(),
 )
