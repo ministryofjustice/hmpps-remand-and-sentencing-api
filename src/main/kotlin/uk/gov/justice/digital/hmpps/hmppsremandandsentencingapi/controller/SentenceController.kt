fce79c1e02e7f49efb9ba42c1616e41830aac160
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2026-09-02 15:06:39 +0100

    RASS-2522: Fix import order in `SentenceController`

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/SentenceController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsremandandsentencingapi/controller/SentenceController.kt
@@ -8,8 +8,8 @@ import jakarta.persistence.EntityNotFoundException
 import jakarta.validation.Valid
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
